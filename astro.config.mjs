ce0591007d7354561180cb0d1643e762017ec6f1
Ray Xia <xia.t@wustl.edu>
2024-10-20 13:24:05 -0400

    base

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://xiatiantian24.github.io',
-  base: '/project-showcase',
+  site: 'https://xiatiantian24.github.io'
 });
